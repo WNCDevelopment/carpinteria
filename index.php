b72c9915e06da4f9c85b0d88793e891906367a0c
Romina Suarez <rominas@vsisoft.com>
2013-12-22 11:05:52 -0200

    First commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+    <script type="text/javascript" src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
+    <link rel="stylesheet" href="styles/css/main.css" />  
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+</head>
+<body>
+    <div id="container" class="main container">
+        <div class="header">
+            <div class="transparent"> 
+                <a class="logo" href="#"><h1>Carpinteria Gonzalo</h1></a>
+            </div>
+        </div>
+        <div id="album" class="album transparent">           
+            <a href="#prev" class="btn-prev"><img src="styles/images/arrow-left.png" /></a>
+            <ul>
+                <li>
+                    <img src="http://mybuilder-uploads.s3.amazonaws.com/2_thumb/463529_fda30284db.jpg" >                
+                </li>
+            </ul>
+            <a href="#next" class="btn-next"><img src="styles/images/arrow-right.png" /></a>
+        </div>
+        <div class="contact-icon"><a href="#contact"><img src="styles/images/email.png"/></a></div>
+    </div>
+</body>
+</html>
