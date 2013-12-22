c18cbdcf73ba939d975e936a460b82e9d469aeb4
Juan Techera <sinneruy@gmail.com>
2013-12-22 18:38:19 -0200

    Merge origin/master
    Conflicts:
    	index-new.php

--- index.php
+++ index.php
@@ -14,67 +14,73 @@
         <![endif]-->
     </head>
     <body>
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-10">
-                    <a class="logo" href="#"><h1>Carpinteria Gonzalo</h1></a>
-                </div>
-                <div class="col-xs-2 contact-icon">
-                    <a data-toggle="modal" href="#contact"><img src="styles/images/email.png"/></a>
-                </div>
-            </div>
-            <div class="row">
-                <div id="slideshow-principal" class="carousel slide" data-ride="carousel">
-                    <!-- Indicators -->
-                    <ol class="carousel-indicators">
-                      
-                    </ol>
-                    <!-- Wrapper for slides -->
-                    <div class="carousel-inner">
-                    
-                    </div>
+        <div class="sketch-1">
+            <div class="sketch-2">
+                <div class="sketch-3">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-xs-10">
+                                <a class="logo" href="#"><h1>Carpinteria Gonzalo</h1></a>
+                            </div>
+                            <div class="col-xs-2 contact-icon">
+                                <a data-toggle="modal" href="#contact"><img src="styles/images/email.png"/></a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div id="slideshow-principal" class="carousel slide" data-ride="carousel">
+                                <!-- Indicators -->
+                                <ol class="carousel-indicators">
 
-                    <!-- Controls -->
-                    <a class="left carousel-control" href="#slideshow-principal" data-slide="prev">
-                        <span class="glyphicon glyphicon-chevron-left"></span>
-                    </a>
-                    <a class="right carousel-control" href="#slideshow-principal" data-slide="next">
-                        <span class="glyphicon glyphicon-chevron-right"></span>
-                    </a>
+                                </ol>
+                                <!-- Wrapper for slides -->
+                                <div class="carousel-inner">
+
+                                </div>
+
+                                <!-- Controls -->
+                                <a class="left carousel-control" href="#slideshow-principal" data-slide="prev">
+                                    <span class="glyphicon glyphicon-chevron-left"></span>
+                                </a>
+                                <a class="right carousel-control" href="#slideshow-principal" data-slide="next">
+                                    <span class="glyphicon glyphicon-chevron-right"></span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Modal -->
+                    <div class="modal fade" id="contact" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                    <h4 class="modal-title" id="myModalLabel">Contacto</h4>
+                                </div>
+                                <div class="modal-body">
+                                    <form role="form" >
+                                        <div class="form-group">
+                                            <label for="email">Email</label>
+                                            <input type="email" class="form-control" id="email" name="email" placeholder="Email">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="telefono">Telefono</label>
+                                            <input type="telefono" class="form-control" id="telefono" name="telefono" placeholder="Telefono">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="mensaje">Mensaje</label>
+                                            <textarea name="mensaje" class="form-control" id="mensaje"></textarea>
+                                        </div>
+                                    </form>
+                                    <div id="respuesta"></div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                                    <button type="button" id="contact-send" class="btn btn-primary contact-send">Enviar</button>
+                                </div>
+                            </div><!-- /.modal-content -->
+                        </div><!-- /.modal-dialog -->
+                    </div><!-- /.modal -->
                 </div>
             </div>
         </div>
-        <!-- Modal -->
-        <div class="modal fade" id="contact" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                        <h4 class="modal-title" id="myModalLabel">Contacto</h4>
-                    </div>
-                    <div class="modal-body">
-                        <form role="form" >
-                            <div class="form-group">
-                                <label for="email">Email</label>
-                                <input type="email" class="form-control" id="email" name="email" placeholder="Email">
-                            </div>
-                            <div class="form-group">
-                                <label for="telefono">Telefono</label>
-                                <input type="telefono" class="form-control" id="telefono" name="telefono" placeholder="Telefono">
-                            </div>
-                            <div class="form-group">
-                                <label for="mensaje">Mensaje</label>
-                                <textarea name="mensaje" class="form-control" id="mensaje"></textarea>
-                            </div>
-                        </form>
-                        <div id="respuesta"></div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
-                        <button type="button" id="contact-send" class="btn btn-primary contact-send">Enviar</button>
-                    </div>
-                </div><!-- /.modal-content -->
-            </div><!-- /.modal-dialog -->
-        </div><!-- /.modal -->
     </body>
 </html>
